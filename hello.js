0038bc9a1287eb305b8fb00b30366c2780f98046
gankris123 <ganiemodesto300@gmail.com>
2025-09-09 21:02:44 +0800

    added main menu with options for hello, factorial, and exit

--- hello.js
+++ hello.js
@@ -5,3 +5,11 @@ const rl = readline.createInterface({
     input:process.stdin,
     output: process.stdout
 })
+
+function mainMenu(){
+    console.log("---------------------------");
+    console.log("Factorial Application");
+    console.log("1. Say Hello");
+    console.log("2. Factorial");
+    console.log("3. Exit Application");
+    console.log("---------------------------");
