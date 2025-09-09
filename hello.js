cd697c01940d26fb89b3be8d7e57c70f62778379
gankris123 <ganiemodesto300@gmail.com>
2025-09-09 21:03:57 +0800

    added menu choice with options for hello, factorial, and exit

--- hello.js
+++ hello.js
@@ -13,3 +13,18 @@ function mainMenu(){
     console.log("2. Factorial");
     console.log("3. Exit Application");
     console.log("---------------------------");
+
+     rl.question("Enter your choice (1-3): ", choice =>{
+    
+        if (choice === "1"){
+           sayHello();
+        } else if (choice === "2"){
+           computeFactorial();
+        } else if (choice === "3"){
+            exitProgram();
+        } else {
+            console.log("Invalid Choice. Please Try Again");
+        }
+
+    })
+}
