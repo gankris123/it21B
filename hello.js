e950131f26055c4d5911fcc6a7d79258f240240e
gankris123 <ganiemodesto300@gmail.com>
2025-09-09 21:09:23 +0800

    added sayHello and computeFactorial functions with input validation

--- hello.js
+++ hello.js
@@ -28,3 +28,28 @@ function mainMenu(){
 
     })
 }
+
+function sayHello(){
+    console.log("Hello");
+}
+
+function computeFactorial(){
+    console.log("Factorial");
+
+        rl.question("Please enter a number for factorial: ", numStr =>{
+            let num = parseInt(numStr);
+
+            if (isNaN(num) || num < 0){
+                console.log("Please enter a non-negative integer");
+            } else {
+                let fact = 1;
+
+                for (let i=1; i <= num; i++){
+                    fact *=i;
+                }
+
+                console.log("The factorial of " + num + " is " + fact);
+            }
+            backToMenu();
+        })
+}
