7e0addf41c130dfd8ac0d106def18893e3562ab6
gankris123 <ganiemodesto300@gmail.com>
2025-09-12 08:25:18 +0800

    added showName and showMessage

--- app.js
+++ app.js
@@ -15,10 +15,20 @@ class person{
         console.log("Age: "+ this.name);
         console.log("Occupation: "+ this.occupation);
         console.log("Message: "+ this.message);
+        console.log("-----------------------");
+    }
+    showName(){
+    console.log(this.name);
+}
+    ShowMessage(){
+        console.log(This.message);
+
     }
 }
 
 const person1 = new Person("Coco Martin" ,24,"Masahista","Ser, tapos napo");
 const person2 = new Person("Victor Magtanggol",24,"Superhero","Arjooooooooooooo");
 
+person1.displayInfo();
 person2.displayInfo();
+
