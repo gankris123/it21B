86e9ad064e06b198b702ad12b0faadbf481ce20c
gankris123 <ganiemodesto300@gmail.com>
2025-09-12 08:17:43 +0800

    added new message Coco Martin

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("This is app.js");
+class person{
+    name;
+    age;
+    occupation;
+    message;
+
+    constructor(name, age, occupation, message){
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.message = message;
+    }
+    displayInfo(){
+        console.log("Name:"+ this.name);
+        console.log("Age: "+ this.name);
+        console.log("Occupation: "+ this.occupation);
+        console.log("Message: "+ this.message);
+    }
+}
+
+const person1 = new Person("Coco Martin" ,24,"Masahista","Ser, tapos napo");
+const person2 = new Person("Victor Magtanggol",24,"Superhero","Arjooooooooooooo");
+
+person2.displayInfo();
