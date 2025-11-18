5d12fea6c29e83766244033928f49891d3e63cf1
gankris123 <ganiemodesto300@gmail.com>
2025-11-18 08:25:48 +0800

    added a grade computation

---
+++ math.js
@@ -0,0 +1,61 @@
+function sum(num1,num2){
+    sum = num1 + num2;
+    console.log(sum);
+}
+
+sum(2,3);
+ function diff(num1,num2){
+    diff = num1 - num2;
+    console.log(diff);
+ }
+
+ diff(2,3);
+
+ function prod(num1,num2){
+    prod = num1 * num2;
+    console.log(prod);
+ }
+
+ prod(2,3);
+
+ function quo(num1,num2){
+     quo = num1 / num2;
+     console.log(quo);
+
+ }
+
+ /*
+ sum(2,3);
+ diff(2,3);
+ prod(2,3);
+ quo(2,3);
+ */
+function add(num1,num2){
+    return num1 + num2;
+}
+
+
+
+
+function
+      
+
+
+
+
+
+
+
+
+
+
+
+
+function add(num1,num2){
+    return num1 + num2;
+}
+
+ console.log(add(2,3));
+ console.log(substract(2,3);
+ console.log(multiply(2,3));
+ console.log(divide(2,3));
